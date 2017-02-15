52e59394712d2449b3ed165b50403f224e9e40e5
mengf1 <meng.fang@unimelb.edu.au>
2017-02-15 21:58:36 +1100

    fix a bug; represent sentences

--- src/gensim-nlp/genLDA.py
+++ src/gensim-nlp/genLDA.py
@@ -11,10 +11,13 @@ documents = ["Apple is releasing a new product",
 stoplist = set('for a of the and to in'.split())
 texts = [[word for word in document.lower().split() if word not in stoplist]
          for document in documents]
-dictionary = corpora.Dictionary(texts)
+dictionary = gensim.corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 lda = gensim.models.ldamodel.LdaModel(
     corpus=corpus, id2word=dictionary, num_topics=5, update_every=1, chunksize=10000, passes=1)
 
+# represent a sentence
+doc_bow = corpus[0]
 doc_lda = lda[doc_bow]
+print doc_lda
